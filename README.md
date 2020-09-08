bebf41b47ce519e5ac26b309456ab14303841c11
Nicolas-Crommen <64846370+Nicolas-Crommen@users.noreply.github.com>
2020-09-08 11:38:48 +0200

    Final

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Nico-CLI
+
+Holidates allows you to retrieve the holidays for any given country or year.
+
+Simply type "holidates + country + year" in the terminal and you'll get the list. Enjoy !
+
+If you don't specify a year, you'll get the current year holidays.
+
+To install it : npm install @nicolas1576/holidates
+
+Thank you !
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 #!/usr/bin/env node
 
-var myArgs = process.argv.slice(2);
-console.log('myArgs: ', myArgs);
+let country=process.argv[2];
+
+let date=new Date();
+let year=date.getFullYear();
+
+if (process.argv[3]!=undefined) {
+	year=process.argv[3];
+}
 
 const { getCode, getName } = require('country-list');
-console.log(getName('IS'));
+let country_code = getCode(country);
 
-console.log("Hello, Node.JS!");
+const axios = require("axios");
+
+async function getHolyDays() {
+	const response = await axios.get("https://date.nager.at/api/v2/publicholidays/"+year+"/"+country_code);
+
+let results = response.data;
+results.forEach(function(result) {
+	console.log(result.date + " : " + result.name);
+});
+
+}
+
+getHolyDays()
